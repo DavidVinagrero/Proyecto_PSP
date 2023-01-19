b3905e5d5621ffa4d72d2bae02c7beea9030f37c
DavidVinagrero <david.i.vinagrero.e@gmail.com>
2023-01-19 20:42:55 +0100

    update

--- Proyecto_Flask/application.py
+++ Proyecto_Flask/application.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 
 application = Flask(__name__)
 
 
 @application.route('/')
 def hello_world():
-    return "<h1>Hola mundo</h1>"
+    print(request.method)
+    return "<h1>Hello world</h1>"
+
+@application.post('/')
+def hello_world_post():
+    print('Hola desde post')
+    return 'Hola'
+
+@application.route('/usuarios/<username>')
+def return_username(username):
+    return username
