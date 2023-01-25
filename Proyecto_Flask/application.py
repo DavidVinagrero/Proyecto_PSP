c5fd07ae7f637c49718422881bbcd0a830b891a6
DavidVinagrero <david.i.vinagrero.e@gmail.com>
2023-01-25 20:19:34 +0100

    update

--- Proyecto_Flask/application.py
+++ Proyecto_Flask/application.py
@@ -1,9 +1,49 @@
 from flask import Flask, request
+from flask import render_template
+from flask_cors import CORS
 
 application = Flask(__name__)
+CORS(application)
+
+@application.post('/create-todo')
+def create_todo():
+    pass
+
+@application.route('/get-todos')
+def get_todos():
+    h = [[1,"Todo1",True],[2,"Todo2",False],[3,"Todo3",False]]
+    i = []
+    i.append({"id":h[0][0],"todo":h[0][1],"checked":h[0][2]})
+    i.append({"id":h[1][0],"todo":h[1][1],"checked":h[1][2]})
+    i.append({"id":h[2][0],"todo":h[2][1],"checked":h[2][2]})
+
+    data = [
+        {
+            "id": 0,
+            "Todo":"primer todo",
+            "comleted": True
+        },
+        {
+            "id": 1,
+            "Todo":"primer todo",
+            "comleted": True
+        },
+        {
+            "id": 2,
+            "Todo":"primer todo",
+            "comleted": False
+        }
+    ]
+
+    return i # o data
+
+@application.put('/complete-todo')
+def complete_todo():
+    pass
 
 
-@application.route('/')
+
+""" @application.route('/')
 def hello_world():
     print(request.method)
     return "<h1>Hello world</h1>"
@@ -15,4 +55,4 @@ def hello_world_post():
 
 @application.route('/usuarios/<username>')
 def return_username(username):
-    return username
+    return render_template('hello.html', username=username) """
