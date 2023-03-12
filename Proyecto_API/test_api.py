66f268f5fc0b22eddcc9c9abb5dad5ce29b4ff6c
DavidVinagrero <113194774+DavidVinagrero@users.noreply.github.com>
2023-03-12 11:07:22 +0100

    update

--- Proyecto_API/test_api.py
+++ Proyecto_API/test_api.py
@@ -6,4 +6,4 @@ import requests
 
 # req = requests.put('http://127.0.0.1:5000/modificar-objeto?nombre=Corchetee&nuevo_nombre=Corchete&calidad=C')
 # req = requests.put('http://127.0.0.1:5000/modificar-arma?nombre=A.W.P&calidad=A')
-req = requests.put('http://127.0.0.1:5000/modificar-jefe?nombre=Rey Bala&piso=1')
+# req = requests.put('http://127.0.0.1:5000/modificar-jefe?nombre=Rey Bala&piso=1')
